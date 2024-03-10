de496652700bbda66094d4e5538996793633eae5
Filip Spaniał <s24944@pjwstk.edu.pl>
2024-03-10 18:42:56 +0100

    Modyfikacja 1

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Zadanie1;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
